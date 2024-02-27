e8a14d85aac3217e4dc39b7997f7d5a5c4094cc6
lewisFarley403 <32036865+lewisFarley403@users.noreply.github.com>
2024-02-27 12:42:28 +0000

    Update Stage.java

--- src/cycling/Stage.java
+++ src/cycling/Stage.java
@@ -5,18 +5,19 @@ import java.util.HashMap;
 public abstract class Stage {
     private StageType stageType;
     public int[] riders;
-    private HashMap<Integer, Float> results = new HashMap<>(); //riderID as keys, times as values
+    private HashMap<Integer, Float> results; //riderID as keys, times as values
 
     public Stage(StageType stageType,int[] riders){
         this.stageType = stageType;
         this.riders = riders;
+        this.results = new HashMap<>();
+        for(int riderID:riders){
+            //add riderID,0 key pair to this.
+            this.addRider(riderID);
+        }
     }
-    public void removeRider(int ID){
-
-    }
-    public void addRider(int ID){
-
-    }
+    public abstract void removeRider(int ID);//abstract needs to be implemented
+    public abstract void addRider(int ID); //abstract, needs to be...
     public abstract HashMap<Integer,Float>getGCPoints();
     public abstract HashMap<Integer,Float>getSprinterPoints();
     public float getGCPointsByID(int ID){
@@ -24,8 +25,8 @@ public abstract class Stage {
         return totalGCs.get(ID); //catch errors here
     }
     public float getSprinterPointsByID(int ID){
-        HashMap<Integer,Float> totalSpitner = this.getSprinterPoints();
-
+        HashMap<Integer,Float> totalSprinter = this.getSprinterPoints();
+        return totalSprinter.get(ID);
 
     }
 }
